2a882df1f4134774c8f71da387c595463f8e7829
Tien Phan <tienphan1989@gmail.com>
2020-05-03 19:21:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if (src[row_index][element_index].kind_of? == String) 
+    if (src[row_index][element_index].is_string?) 
     new_array.push(src[row_index][element_index])
   end
     element_index += 1
